4314bdfa2dd0f76604af521573e5269d9208a9d1
Darcy <Darcelisgutierrez@gmail.com>
2021-02-19 12:13:56 -0500

    Pause working in level 3 + captive G.O implemented

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -29,10 +29,13 @@ public class ButtonManager : MonoBehaviour
     public void Options()//open up options panel works for both in game and in title
     {
         optionsPanel.SetActive(true);
+       
     }
-    public void BackToTitle()//from whatever things youre looking at go back to the title screen
+    public void BackToTitle()//from whatever things youre looking at go back to the title/start screen
     {
         optionsPanel.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TitleScene");
     }
     public void PauseToMain()//from the pause menu head back to the start screen
     {
