8a0d179db5600d3a13a36707cad6910bccc4ff08
Wynta <wyntajc@gmail.com>
2021-02-19 13:41:22 -0500

    Working on magnet controls

--- Assets/Scripts/MagnetScript.cs
+++ Assets/Scripts/MagnetScript.cs
@@ -6,29 +6,44 @@ public class MagnetScript : MonoBehaviour
 {
     public PlayerBehavior player;
     public Vector3 move;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindObjectOfType<PlayerBehavior>();
-
         move.x = player.move.x;
         move.y = player.move.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        transform.Translate(move);
+        Debug.Log(player.move);
+        transform.Translate(move * Time.deltaTime * speed);
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "wall")
         {
-            player.Move();
+            Debug.Log("a");
+            player.canmove = true;
             move.x = 0;
             move.y = 0;
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "wall")
+        {
+            move.x = player.move.x;
+            move.y = player.move.y;
+            player.canmove = false;
+        }
+        
     }
 }
